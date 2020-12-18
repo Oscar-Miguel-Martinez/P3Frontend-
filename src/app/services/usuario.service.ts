ca7114f79c9880f3c87537ec975477c62d744528
Oscar Miguel Martinez <omiguelomar@gmail.com>
2020-12-18 14:31:56 -0600

    frontend

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RegisterForm } from '../interfaces/register-form.interface';
+import { environment } from '../../environments/environment';
+import { catchError, map, tap } from 'rxjs/operators';
+import { LoginForm } from '../interfaces/login-form.interface';
+import { Observable, of } from 'rxjs';
+import { PasswordForm } from '../interfaces/updatepass-form.interface';
+
+const base_url = environment.base_url;
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  constructor(private http: HttpClient) { }
+
+  crearUsuario(formData: RegisterForm){
+    return this.http.post(`${base_url}/usuarios`, formData);
+  }
+
+  login(formData: LoginForm){
+    return this.http.post(`${base_url}/login`, formData).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp.data);
+      })
+    );
+  }
+
+  loginGoogle(token){
+    return this.http.post(`${base_url}/login/google`, { token }).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp.data);
+      })
+    );
+  }
+
+  renovarToken():Observable<boolean> {
+    const token = localStorage.getItem('token') || '';
+    return this.http.get(`${base_url}/login/renovartoken`, {
+      headers: {
+        'x-token': token
+      },
+    }).pipe(
+      tap((resp: any) => {
+        localStorage.setItem('token', resp.data);
+      }),
+      map((resp) => {
+        if(resp.data)
+          return true;
+        else
+          return false;
+      }),
+      catchError((error) => of(false))
+    );
+  }
+
+  recuperarPassword(formData: PasswordForm) {
+    return this.http.put(`${base_url}/usuarios/updatepassword`, formData);
+  }
+
+}
